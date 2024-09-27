8fd35d4f83930e0999c9c2aff16adedc650fb039
= <evc1evcus@gmail.com>
2024-09-27 21:10:37 +0300

    Site toolbar has been added

---
+++ src/Components/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Navbar, Nav, FormControl, Container, Form, Button } from "react-bootstrap";
+import logo from "./aoh_icon.ico"
+
+export default class Header extends Component {
+    render() {
+        return (
+            <>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/"> Home </Nav.Link> 
+                            <Nav.Link href="/play"> Start play </Nav.Link>
+                            <Nav.Link href="/about"> About us </Nav.Link>
+                            <Nav.Link href="/blog"> Blog </Nav.Link>
+                        </Nav>
+                        <Form className='d-flex'>
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="flex-grow-1 me-2"
+                            />
+                            <Button variant="outline-info" className="ms-auto"> Search </Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            </>
+        )
+    }
+}
