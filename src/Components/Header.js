f660011128687fdfa5ed6b630d92108dd5f0caf8
= <evc1evcus@gmail.com>
2024-09-29 15:23:36 +0300

    Navbar changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,81 @@
 import React, { Component } from "react";
 import { Navbar, Nav, FormControl, Container, Form, Button } from "react-bootstrap";
 import logo from "./aoh_icon.ico"
+import styled from 'styled-components';
+
+const StyledNavbar = styled(Navbar)`
+    height: 65px;
+    font-size: 16px;
+    font-weight: bold;
+    background-color: rgba(0, 0, 0, 0.7);
+    backdrop-filter: blur(10px);
+    .navbar-brand, .nav-link {
+        color: #fff;
+    }
+    .nav-link:hover {
+        color: #ccc;
+    }
+`;
+
+const NavbarContent = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 75px;
+`;
+
+const StyledButtonSignUp = styled(Button)`
+    font-size: 16px;
+    font-weight: bold;
+    background-color: #fff;
+    border-color: #ddd;
+    &:hover {
+        background-color: #ccc;
+        border-color: #bbb;
+        color: #333;
+    }
+`;
+
+const StyledButtonSignIn = styled(Button)`
+    font-size: 16px;
+    font-weight: bold;
+    border: none;
+    color: #fff;
+    &:hover {
+        color: #ccc;
+    }
+`;
 
 export default class Header extends Component {
     render() {
         return (
-            <>
-            <Navbar collapseOnSelect fixed="top" expand="md" bg="dark" variant="dark">
-                <Container>
+            <StyledNavbar collapseOnSelect fixed="top" expand="md">
+                <NavbarContent>
                     <Navbar.Brand href="/">
                         <img
                             src={logo}
-                            height="30"
-                            width="30"
+                            height="35"
+                            width="35"
                             className="d-inline-block align-top"
                             alt="Logo"
                         />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto">
+                        <Nav className="me-auto">
                             <Nav.Link href="/"> Home </Nav.Link> 
                             <Nav.Link href="/play"> Start play </Nav.Link>
                             <Nav.Link href="/about"> About us </Nav.Link>
                             <Nav.Link href="/blog"> Blog </Nav.Link>
                         </Nav>
-                        <Form className='d-flex'>
-                            <FormControl
-                                type="text"
-                                placeholder="Search"
-                                className="flex-grow-1 me-2"
-                            />
-                            <Button variant="outline-info" className="ms-auto"> Search </Button>
-                        </Form>
+                        <Nav>
+                            <StyledButtonSignIn variant="outlined" className="me-2"> Sign in </StyledButtonSignIn>
+                            <StyledButtonSignUp variant="outlined"> Sign up </StyledButtonSignUp>
+                        </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            </>
+                </NavbarContent>
+            </StyledNavbar>
         )
     }
 }
